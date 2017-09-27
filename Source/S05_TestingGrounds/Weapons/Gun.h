a558a1a27d617ab8a43af898eca240b9795c4383
IGORGOLIATH\Igor <ovo@wp.pl>
2017-09-27 15:03:30 +0200

    Firing NPCs, NPC death animation

--- Source/S05_TestingGrounds/Weapons/Gun.h
+++ Source/S05_TestingGrounds/Weapons/Gun.h
@@ -31,6 +31,7 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	/** Fires a projectile. */
+	UFUNCTION(BlueprintCallable, Category = "Input")
 	void OnFire();
 
 	/** Projectile class to spawn */
